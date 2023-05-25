37e903ab8eae9d0b11d7166ae9e96795ea01c4cf
vishal <vishalkumar18ssb786@gmail.com>
2023-05-25 16:38:51 +0530

    -

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb+srv://vishal:123@cluster0.sula7wu.mongodb.net/`)
+// mongoose.connect(`mongodb+srv://vishal:123@cluster0.sula7wu.mongodb.net/`)
+mongoose.connect('',{useNewUrlParser:true, useUnifiedTopology: true})
 .then(() => console.log('🔥'))
 .catch((err) => console.log(`error ${err}`))
 
